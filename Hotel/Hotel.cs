1373e9247173466c41f323aea86b791aaab14886
Sebastian Ruszkiewicz <losiek2601@gmail.com>
2022-07-19 16:16:53 +0200

    Add project files.

---
+++ Hotel/Hotel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static OMenu.OwnerMenu;
+
+namespace BetterBook // Note: actual namespace depends on the project name.
+{
+    internal class Hotel
+    {
+        
+
+         static void Main(string[] args)
+        {
+            int userInput = 999;
+            Console.WriteLine("Witaj! \n");
+            while (userInput != 0)
+            {
+                
+                Console.WriteLine("Wybierz jedną z opcji: \n");
+                Console.WriteLine("1 - Zaloguj się");
+                Console.WriteLine("2 - Przeglądaj");
+                Console.WriteLine("0 - Wyjście");
+
+                string pw = "123";
+                userInput = int.Parse(Console.ReadLine());
+
+                switch (userInput)
+                {
+                    case 1:
+                        Console.WriteLine("Wprowadź Hasło: ");
+                        string pwCheck = Console.ReadLine();
+                        if (pwCheck == pw)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Pomyślnie Zalogowano! \n");
+                            OMenu.OwnerMenu.stuff(userInput);
+
+                        }
+                        else Console.WriteLine("Złe hasło! \n");
+
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Druga opcja");
+
+
+                        break;
+                    
+                    default: 
+                        Console.WriteLine("Wybierz jeszcze raz \n");
+                        break;
+
+                }
+            };
+            
+            //Console.Clear();
+        }
+    }
+}
---
+++ Hotel/OwnerMenu.cs
@@ -0,0 +1,20 @@
+﻿using static BetterBook.Hotel;
+
+namespace OMenu
+{
+    public class OwnerMenu
+    {
+        public static void stuff(int userInput)
+        {
+            Console.WriteLine("Co byś chciał zrobić? \n");
+            Console.WriteLine("1 - Dodaj Atrakcję \n");
+            Console.WriteLine("2 - Zobacz istniejące atrakcje \n");
+            Console.WriteLine("3 - Usuń atrakcje \n");
+            Console.WriteLine("0 - Wyjście \n");
+
+            userInput = int.Parse(Console.ReadLine());
+
+        }
+        
+    }
+}
